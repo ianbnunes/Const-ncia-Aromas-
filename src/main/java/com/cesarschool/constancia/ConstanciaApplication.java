b3e3e9fa8f4dd0d3cbad9c011ca369edee62ebfc
Ian Nunes <ianbnunes@outlook.com>
2024-11-14 11:34:59 -0300

    first commit

---
+++ src/main/java/com/cesarschool/constancia/ConstanciaApplication.java
@@ -0,0 +1,12 @@
+package com.cesarschool.constancia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstanciaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConstanciaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cesarschool/constancia/ConstanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarschool.constancia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstanciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
